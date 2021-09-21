7242215652fc81546a23299c1ceaa38822b2502d
Thomas Brouillet <thomasbrouillet@gmail.com>
2021-09-21 23:15:15 +0200

    app structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import "./style/style.css";
+
+import React, { useState } from "react";
+
+import QuoteCard from "./components/QuoteCard";
+
 function App() {
+  const [data, setData] = useState([
+    {
+      quote:
+        "These are my only friends...grown-up nerds like Gore Vidal. And even he's kissed more boys than I ever will.",
+      character: "Lisa Simpson",
+      image:
+        "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083",
+      characterDirection: "Right",
+    },
+  ]);
+
   return (
-    <div className="App">
-      <div>TODO</div>
+    <div className="container">
+      <button>New Quote</button>
+      {data.map((quote, index) => (
+        <QuoteCard key={index} {...quote} />
+      ))}
     </div>
   );
 }
---
+++ src/components/QuoteCard.js
@@ -0,0 +1,15 @@
+import "../style/QuoteCard.css";
+
+import React from "react";
+
+const QuoteCard = ({ quote, character, image }) => (
+  <figure className="QuoteCard">
+    <img src={image} alt={character} />
+    <figcaption>
+      <blockquote>{quote}</blockquote>
+      <cite>{character}</cite>
+    </figcaption>
+  </figure>
+);
+
+export default QuoteCard;
